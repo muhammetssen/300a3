622a34894df3ecb3d1bea51abc47aaeb41756c7f
Muhammet Şen <enmuhammet23@gmail.com>
2022-01-16 20:41:25 +0300

    removed result_mini from main.py

--- main.py
+++ main.py
@@ -42,35 +42,14 @@ def part1():
     def runner():
         for value in values:
             count = 0
-            s = 0
-            us = 0
             for i in range(10000):
                 solution, logs = QueensLasVegas(value)
                 with open(f"result_{value}.txt", 'a+') as f:
                     if solution == None:
                         f.write("Unsuccessful\n")
-                        if us < 2:
-                            us += 1
-                            with open(f"result_mini.txt", 'a+') as mini_f:
-                                mini_f.write(f"n={value}, ")
-                                mini_f.write("Unsuccessful:\n")
-                                mini_f.write("\n".join(logs))
-                                mini_f.write("\n\n")
-                                mini_f.close()
 
                     else:
                         f.write("Successful\n")
-                        if s < 2:
-                            s += 1
-                            with open(f"result_mini.txt", 'a+') as mini_f:
-                                mini_f.write(f"n={value}, ")
-                                mini_f.write("Successful:\n")
-                                mini_f.write("\n".join(logs))
-                                mini_f.write("\n")
-                                mini_f.write(
-                                    f"Step {value+1}: Columns = {solution}\n")
-                                mini_f.write("\n")
-                                mini_f.close()
                     f.write("\n".join(logs))
                     f.write("\n")
                     if solution != None:
